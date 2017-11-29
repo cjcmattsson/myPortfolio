6ee30b0ff94e79ee28445ba8ce2a33145ff0d864
cjcmattsson <cjcmattsson@gmail.com>
2017-11-29 23:04:30 +0100

    PLayed with hover css + js

--- second/scripts/animations.js
+++ second/scripts/animations.js
@@ -1,3 +1,5 @@
+// Have the phone slide in from the right on page load
+
 function phoneUp() {
   const phone = document.querySelector('.iphone');
 
@@ -6,6 +8,21 @@ function phoneUp() {
   });
 };
 
-
-
 window.onload = phoneUp();
+
+
+// Lower opacity on hovered elements siblings
+
+// FRÅGA ELINA OM DETTA
+// SE CCS'sen också!!!
+
+ const projectsExample = document.querySelectorAll('.projects-example');
+ const first = document.querySelectorAll('.first');
+
+ console.log(projectsExample);
+
+
+
+projectsExample.addEventListener('mouseover', function(event){
+    first.classList.add('black');
+  });
