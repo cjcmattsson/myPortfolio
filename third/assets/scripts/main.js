c9f1477fbe4868062c887d0bc8dc14e2b01561fb
cjcmattsson <cjcmattsson@gmail.com>
2018-02-05 12:06:52 +0100

    colored menu

--- third/assets/scripts/main.js
+++ third/assets/scripts/main.js
@@ -1,6 +1,5 @@
 const scroll = document.querySelector('.scroll');
 const name = document.querySelector('h1');
-const middle = document.querySelector('.middle-section');
 const textLanding = document.querySelector('.text-content-landing');
 const contact = document.querySelector('.button');
 const contactMenu = document.querySelector('.contact-menu');
@@ -24,7 +23,7 @@ const about = document.querySelector('#about');
 console.log(window.scrollY);
 
 scroll.addEventListener('click', () => {
-  middle.scrollIntoView({
+  portfolio.scrollIntoView({
     behavior:"smooth",
     block:"start"
   });
@@ -61,20 +60,20 @@ const sticky = navbar.offsetTop;
 
 window.addEventListener('scroll', function() {
 
-  const top = middle.getBoundingClientRect().top;
+  const top = portfolio.getBoundingClientRect().top;
   console.log(top);
 
-  if (top <= 550 && !textLanding.classList.contains('hide-landing')) {
+  if (top <= 430 && !textLanding.classList.contains('hide-landing')) {
     textLanding.classList.add('hide-landing');
   }
-   if (top > 570 && textLanding.classList.contains('hide-landing')) {
+   if (top > 250 && textLanding.classList.contains('hide-landing')) {
     textLanding.classList.remove('hide-landing');
   }
 
-  if (top <= 550 && !scroll.classList.contains('hide-landing')) {
+  if (top <= 430 && !scroll.classList.contains('hide-landing')) {
     scroll.classList.add('hide-landing');
   }
-   if (top > 570 && scroll.classList.contains('hide-landing')) {
+   if (top > 250 && scroll.classList.contains('hide-landing')) {
     scroll.classList.remove('hide-landing');
   }
 
