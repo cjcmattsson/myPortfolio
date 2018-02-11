119873acefce586147a20006722e8e7db63244d6
cjcmattsson <cjcmattsson@gmail.com>
2018-02-11 17:30:31 +0100

    Fixed loading page

--- third/assets/scripts/loading.js
+++ third/assets/scripts/loading.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const loadingPage = document.querySelector('.loading-page');
 const loader = document.querySelector('.loader');
 
-function stopScroll() {
+function startLoading() {
   body.classList.add('no-scroll');
   setTimeout(() => {
     body.classList.remove('no-scroll');
@@ -15,6 +15,15 @@ function stopScroll() {
   }, 1800);
 }
 
-window.onload = stopScroll;
+// If user visits page for first time/after closed a window, execute loadingPage
+// If user reloads page, dont do loadingPage
+if (sessionStorage.getItem('key') === 'firstVisit') {
+  loadingPage.classList.add('remove-loading');
+  loader.classList.add('remove-loading');
+  console.log("hej");
+} else {
+  window.onload = startLoading;
+}
 
+sessionStorage.setItem('key', 'firstVisit');
 window.onbeforeunload = function(){ window.scrollTo(0,0); }
